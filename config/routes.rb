286e7fc6c44f24f504912a29c72ba8479a1830eb
linjiansi <pmon.ken.1467@gmail.com>
2021-03-31 15:38:28 +0900

    Add books paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
       post '/login',    to: 'users#login'
       post '/sign_up',  to: 'users#sign_up'
       delete '/logout', to: 'users#logout'
+
+      get '/books',     to: 'books#fetch_books'
+      post '/books',    to: 'books#add_book'
+      get '/books/:id', to: 'books#fetch_book'
+      put '/books/:id', to: 'books#edit_book'
     end
   end
 end
