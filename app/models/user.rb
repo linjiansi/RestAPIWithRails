d9d99dfded1d00bb13ab54bb251d2a93b7a88318
linjiansi <pmon.ken.1467@gmail.com>
2021-03-31 11:23:48 +0900

    Add email and password validation and the token setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]\z/i
+
+  has_secure_token :token, length: 36
+
+  validates :email,
+            length: { minimum: 6 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
+
+  validates :password, length: { minimum: 6 }
 end
