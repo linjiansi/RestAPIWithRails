1d7c39c19cf08d88ecca0b67df643a05e2b0dc61
linjiansi <pmon.ken.1467@gmail.com>
2021-04-04 22:17:31 +0900

    Modify the users_controller to adapt the JWT

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,70 +1,46 @@
 class Api::V1::UsersController < ApplicationController
 
-  before_action :authenticate, only: [:logout]
+  before_action :authorized_checker, only: [:logout]
 
   def login
-    login_user = User.find_by(user_param)
-    if login_user.present?
+    login_user = User.find_by(email: params[:email])
+    if login_user && login_user.authenticate(params[:password])
+      token = encode_token({ user_id: login_user.id })
       render json: {
           status: 200,
           result: {
               id: login_user.id,
               email: login_user.email,
-              token: login_user.token
+              token: token
           }
       }
     else
-      render json: {
-          result: {
-              message: "認証エラーです",
-              description: "リクエストをご確認ください"
-          }
-      }
+      authorize_error
     end
   end
 
   def sign_up
     sign_up_user = User.create(user_param)
     if sign_up_user.present?
+      token = encode_token({user_id: sign_up_user.id })
       render json: {
           status: 200,
           result: {
               id: sign_up_user.id,
               email: sign_up_user.email,
-              token: sign_up_user.token
+              token: token
           }
       }
     else
-      render json: {
-          result: {
-              message: "認証エラーです",
-              description: "リクエストをご確認ください"
-          }
-      }
+      authorize_error
     end
   end
 
   def logout
-    if @auth_user.present?
-      @auth_user.update(token: null)
-      render json: { status: 200 }
-    else
-      render json: {
-          result: {
-              message: "見つかりません",
-              description: "システム管理者にお問い合わせください"
-          }
-      }
-    end
+    render json: { status: 200 }
   end
 
   private
-
-  def authenticate
-    authenticate_or_request_with_http_token do |token, options|
-      @auth_user = User.find_by(token: token)
-    end
-  end
 
   def user_param
     params.permit(:email, :password)
