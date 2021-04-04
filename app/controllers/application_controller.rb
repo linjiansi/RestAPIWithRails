b5cc6c7212d0ee71d523a405132154b59cda9c11
linjiansi <pmon.ken.1467@gmail.com>
2021-04-04 22:14:54 +0900

    Configure authentication about token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,50 @@
 class ApplicationController < ActionController::API
+
+  def encode_token(palyload)
+    JWT.encode(palyload, 'shoshosdodh')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'shoshosdodh', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def logged_in_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized_checker
+    render json: {
+        result: {
+          message: '見つかりません',
+          description: 'システム管理者にお問い合わせください'
+        }
+    } unless logged_in?
+  end
+
+  def authorize_error
+    render json: {
+        result: {
+            message: "認証エラーです",
+            description: "リクエストをご確認ください"
+        }
+    }
+  end
 end
