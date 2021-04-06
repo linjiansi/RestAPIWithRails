c81cd6d6709d10678ab8bf9249e01a02ace0621e
linjiansi <pmon.ken.1467@gmail.com>
2021-04-06 14:47:47 +0900

    Configure the book model to fetch a image url before a image data saving to db

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  belongs_to :users, optional: true
+  before_create :fetch_book_image_url
+  before_update :fetch_book_image_url
+
+  def fetch_book_image_url
+    binding.pry
+    self.image = Imgur.new.anonymous_upload(image) if image.present?
+  end
 end
