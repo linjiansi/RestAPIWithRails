08ce07ef4f336cdbdcc9ae98c31a9015aa678a51
linjiansi <pmon.ken.1467@gmail.com>
2021-04-09 11:18:39 +0900

    Modify callback methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :users, optional: true
-  before_create :fetch_book_image_url
-  before_update :fetch_book_image_url
+  before_save :fetch_book_image_url
 
   def fetch_book_image_url
     self.image = Imgur.new.anonymous_upload(image) if image.present?
