d4284fa9bd4be0c98f2e5c31884aafe48d159452
linjiansi <pmon.ken.1467@gmail.com>
2021-04-09 11:26:14 +0900

    Add the freeze method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :books
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :email,
             presence: true,
