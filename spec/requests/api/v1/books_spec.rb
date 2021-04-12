18787f04f1dee3379ad16879d90ae3f07eeaf610
linjiansi <pmon.ken.1467@gmail.com>
2021-04-12 09:55:36 +0900

    Add comments in the books_spec

--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Books", type: :request do
+RSpec.describe "Api::V1::Books",
+               type: :request do
+  #ヘッダーにトークンを付与させる部分がわからない状態です。
+  # あと、Imgurでの処理をどのように対応すれば良いかわからないです。
   describe "GET /books" do
-
   end
 
   describe "POST /books" do
