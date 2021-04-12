7df3665df12eeef00b9fd3413ac2c76bf9cd3c46
linjiansi <pmon.ken.1467@gmail.com>
2021-04-12 09:54:22 +0900

    Add validation tests and a new line in the user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User,
+               type: :model do
   describe "relation" do
     it { should have_many(:books) }
   end
@@ -8,12 +9,12 @@ RSpec.describe User, type: :model do
   describe "validation" do
     context "email" do
       it { should validate_presence_of(:email) }
-      it { should validate_length_of(:email)}
+      it { should validate_length_of(:email).is_at_least(6) }
     end
 
     context "password" do
       it { should validate_presence_of(:password) }
-      it { should validate_length_of(:password) }
+      it { should validate_length_of(:password).is_at_least(6) }
     end
   end
 end
