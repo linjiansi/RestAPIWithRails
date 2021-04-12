7f12e4abf2e802af29463029b0c4b1b7f0551b7b
linjiansi <pmon.ken.1467@gmail.com>
2021-04-12 10:25:23 +0900

    Add a name vaidation test in the book_spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Book,
   describe "relation" do
     it { should belong_to(:user) }
   end
+
+  describe "validation" do
+    context "name" do
+      it { should validate_presence_of(:name) }
+    end
+  end
 end
