48ad34f9c1ddef4c0de790711ef4fc96887ebd41
linjiansi <pmon.ken.1467@gmail.com>
2021-04-13 12:54:08 +0900

    Modify to change a email string to downcase before it is saved to DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :books
 
+  before_save { email.downcase! }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email,
