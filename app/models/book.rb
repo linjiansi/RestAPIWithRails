20ed778b729442a40910cc78b8eec0bfa9948b3c
linjiansi <pmon.ken.1467@gmail.com>
2021-04-14 15:35:03 +0900

    Add book name validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   before_save :convert_book_image_url
 
+  validates :name, presence: true
+
   def convert_book_image_url
     self.image = Imgur.upload(image) if image.present?
   end
