ca6c7afdea33e2fb07c52ea2dd49a499c577510a
linjiansi <pmon.ken.1467@gmail.com>
2021-04-14 10:17:42 +0900

    Rename a method in the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
-  before_save :fetch_book_image_url
+  before_save :convert_book_image_url
 
-  def fetch_book_image_url
+  def convert_book_image_url
     self.image = Imgur.upload(image) if image.present?
   end
 end
