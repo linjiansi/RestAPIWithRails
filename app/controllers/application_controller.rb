3978b6de6494331bb60ae5e3c6a54cd978f3e22a
linjiansi <pmon.ken.1467@gmail.com>
2021-04-14 10:12:28 +0900

    Refactor the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   include JwtManager
 
+  rescue_from StandardError, with: :internal_error
+
   def logged_in_user
     User.find_by(id: decoded_token[0]["user_id"]) if decoded_token
   end
@@ -25,6 +27,4 @@ class ApplicationController < ActionController::API
     render_error_message(I18n.t("errors.occur_system_error"),
                          I18n.t("errors.contact_system_admin"))
   end
-
-  rescue_from StandardError, with: :internal_error
 end
