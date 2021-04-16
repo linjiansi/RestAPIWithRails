c4f2ec37ea17069bc28ccc37f9411aff7c98f1c2
linjiansi <pmon.ken.1467@gmail.com>
2021-04-16 13:48:04 +0900

    Refactor the authorized_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,11 @@ class ApplicationController < ActionController::API
   rescue_from StandardError, with: :internal_error
 
   def authorized_user
-    begin
-      decoded_token
-    rescue
-      render_error_message(I18n.t("errors.not_found"),
-                           I18n.t("errors.contact_system_admin"),
-                           404)
-    end
-
+    decoded_token
+  rescue StandardError
+    render_error_message(I18n.t("errors.not_found"),
+                         I18n.t("errors.contact_system_admin"),
+                         404)
   end
 
   def render_error_message(message, description, status)
