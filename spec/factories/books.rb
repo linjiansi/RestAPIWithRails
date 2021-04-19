be20e743ecb070e020fce0ee31837faac15d84e1
linjiansi <pmon.ken.1467@gmail.com>
2021-04-19 16:48:47 +0900

    Modify the book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :book do
     name { Faker::Book.title }
-    image { Faker::Internet.url(host: 'i.imgur.com') }
+    image { "" }
     price { Faker::Number.between(from: 1, to: 10000) }
-    purchase_date { Faker::Date.from 5.yaer.ago, to: Date.today }
+    purchase_date { Faker::Date.between(from: 5.years.ago, to: Date.today) }
+
+    user
   end
 end
